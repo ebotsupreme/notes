c24b494d9275d9d0f6b3e5d800ffc46cfae6e566
Eddie Jung <ejung716@gmail.com>
2021-08-27 11:28:49 -0700

    Add constraints to DetailViewController. Update keyboard so that text doesn't get cut off once it reaches the top of the keyboard.

--- Project19-21/DetailViewController.swift
+++ Project19-21/DetailViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet var noteTextView: UITextView!
+    
     var note: Note?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        notificationCenterSettings()
+        
         print("Detail note: \(note)")
     }
-
+    
+    func notificationCenterSettings() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            noteTextView.contentInset = .zero
+        } else {
+            noteTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        noteTextView.scrollIndicatorInsets = noteTextView.contentInset
+        
+        let selectedRange = noteTextView.selectedRange
+        noteTextView.scrollRangeToVisible(selectedRange)
+    }
+    
 }
