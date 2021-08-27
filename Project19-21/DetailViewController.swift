2245dde929aed1db46fad578178d8e779310fc45
Eddie Jung <ejung716@gmail.com>
2021-08-27 11:05:22 -0700

    Add tableView settings. Add create tab button and segue to DetailViewController on click.

--- Project19-21/DetailViewController.swift
+++ Project19-21/DetailViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    var note: Note?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        print("Detail note: \(note)")
     }
 
 }
--- Project19-21/ViewController.swift
+++ Project19-21/ViewController.swift
@@ -8,13 +8,47 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var notes = [Note]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        toolbarItems = barButtonItems()
+        navigationController?.isToolbarHidden = false
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Note", for: indexPath)
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note.title
+        cell.detailTextLabel?.text = "\(note.timeStamp)    \(note.description)"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            let note = notes[indexPath.row]
+            vc.note = note
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func barButtonItems() -> [UIBarButtonItem] {
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let compose = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(create))
+        return [spacer, compose]
+    }
+    
+    @objc func create() {
+        print("create tapped!")
+        performSegue(withIdentifier: "toDetail", sender: self)
+    }
 
 }
 
